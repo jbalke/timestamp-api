d40e1faa323caba404e62e8d1a26410857d9ec25
John Balke <john.balke@live.com.au>
2017-05-03 14:57:41 +0100

    Fix for Herku node.js port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 
 app.get('/', function(request, response) {
   response.send('Please  provide a timestamp as a path parameter.')
