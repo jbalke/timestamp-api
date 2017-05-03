f47d651a6c5556ce7a02214b76d657ceda535811
John Balke <john.balke@live.com.au>
2017-05-03 14:33:51 +0100

    Heroku Release

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
 var express = require('express');
 var app = express();
 
-var PORT = process.env.port || 8080;
+var PORT = process.env.port || 80;
 
 app.get('/', function(request, response) {
-  response.send('Hello World');
+  response.send('Please  provide a timestamp as a path parameter.')
+});
+
+app.get('/:timestamp', function(request, response) {
+  var timestamp = request.params.timestamp;
+
+  response.send(formatTimeStampJSON(timestamp));
 });
 
 app.listen(PORT, function () {
-  console.log('Example app listening on port ' + PORT);
+  console.log('Timestamp API listening on port ' + PORT);
 })
+
+function formatTimeStampJSON(timestamp) {
+  var result = {
+    unix: null,
+    natural: null
+  }
+
+  var date;
+  var options = {year: 'numeric', month: 'long', day: 'numeric'}
+
+  if (!isNaN(parseInt(timestamp))) {
+    date = new Date(parseInt(timestamp));
+  } else {
+    date = new Date(timestamp);
+  }
+
+  if (!isNaN(date.getTime())) {
+    result.unix = date.getTime();
+    result.natural = date.toLocaleString(undefined, options);
+  }
+
+  return result;
+};
