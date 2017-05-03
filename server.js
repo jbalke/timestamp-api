1e35e61419b21317fd196b12ab5817b9b34e168f
John Balke <john.balke@live.com.au>
2017-05-03 13:41:57 +0100

    Add procfile to project

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+var PORT = process.env.port || 8080;
+
+app.get('/', function(request, response) {
+  response.send('Hello World');
+});
+
+app.listen(PORT, function () {
+  console.log('Example app listening on port ' + PORT);
 })
